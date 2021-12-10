95aba8050c2b638d30c819fda501f0fa92a6d1ab
shashidhar-patil <shashidhar.patil@wavelabs.ai>
2021-12-10 22:57:02 +0530

    test(amf): stateless unit test
    Signed-off-by: shashidhar-patil <shashidhar.patil@wavelabs.ai>

--- lte/gateway/c/core/oai/tasks/amf/amf_app_state_manager.h
+++ lte/gateway/c/core/oai/tasks/amf/amf_app_state_manager.h
@@ -29,6 +29,16 @@ extern "C" {
 #include <lte/gateway/c/core/oai/include/state_manager.h>
 
 namespace magma5g {
+constexpr char AMF_NAS_STATE_KEY[] = "amf_nas_state";
+constexpr char AMF_UE_ID_UE_CTXT_TABLE_NAME[] =
+    "amf_app_amf_ue_ngap_id_ue_context_htbl";
+constexpr char AMF_IMSI_UE_ID_TABLE_NAME[] = "amf_app_imsi_ue_context_htbl";
+constexpr char AMF_TUN_UE_ID_TABLE_NAME[]  = "amf_app_tun11_ue_context_htbl";
+constexpr char AMF_GUTI_UE_ID_TABLE_NAME[] = "amf_app_tun11_ue_context_htbl";
+constexpr char AMF_GNB_UE_ID_AMF_UE_ID_TABLE_NAME[] =
+    "anf_app_gnb_ue_ngap_id_ue_context_htbl";
+constexpr char AMF_TASK_NAME[]  = "AMF";
+const int NUM_MAX_UE_HTBL_LISTS = 6;
 /**
  * When the process starts, initialize the in-memory AMF+NAS state and, if
  * persist state flag is set, load it from the data store.
@@ -109,6 +119,9 @@ class AmfNasStateManager
 
   void free_state() override;
 
+  void write_state_to_db() override;
+  status_code_e read_state_from_db() override;
+
  private:
   AmfNasStateManager();
   ~AmfNasStateManager();
@@ -125,8 +138,5 @@ class AmfNasStateManager
    * task terminates
    */
   void create_state() override;
-
-  // Clean-up the in-memory hashtables
-  void clear_amf_nas_hashtables();
 };
 }  // namespace magma5g
